d15b57720d1153029c2753478ea635595d7bac55
saurabh-gaonkar <saurabhg@axioned.com>
2022-02-25 12:34:09 +0530

    js file added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,10 @@
-/* Author: 
+let image_content = document.querySelectorAll('.image-content');
 
-*/
-
-
-
-
-
+image_content.forEach(i=>{
+    i.addEventListener('click',function(){
+        alert(this > a.value);
+    })
+})
 
 
 
