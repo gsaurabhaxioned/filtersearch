399961b1b6da9180e4dc04eac27445e6d0e6fccf
saurabh-gaonkar <saurabhg@axioned.com>
2022-02-25 16:08:40 +0530

    js code formatted

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,13 @@
 let image_content = document.querySelectorAll('.image-content'),
-search = document.querySelector('.search'),
-cake_title = document.querySelectorAll('.cake-title');
-
-
-
-search.addEventListener('keyup', function(){
-
+    search = document.querySelector('.search'),
+    cake_title = document.querySelectorAll('.cake-title');
+search.addEventListener('keyup', function () {
     let search_title = document.querySelector('.search').value.toUpperCase();
-     cake_title.forEach((i,index) => {
-      if(i.innerHTML.toUpperCase().indexOf(search_title) > -1) {
-          image_content[index].style.display = "block"; 
-      }else {
-          image_content[index].style.display = "none";
-      }
-     })
+    cake_title.forEach((i, index) => {
+        if (i.innerHTML.toUpperCase().indexOf(search_title) > -1) {
+            image_content[index].style.display = "block";
+        } else {
+            image_content[index].style.display = "none";
+        }
+    })
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
